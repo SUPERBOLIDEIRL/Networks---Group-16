e985913aa283a1f13d5ef3de4b20949f991e93fd
Osezuah <122998658+Osezuah@users.noreply.github.com>
2023-12-09 22:40:49 -0500

    Menu function added

--- ClientBoard.cpp
+++ ClientBoard.cpp
@@ -1,4 +1,7 @@
 #include <arpa/inet.h>
+#include <limits>
+#include <algorithm>
+#include <cctype>
 #include <iostream>
 #include <cstring>
 #include <unistd.h>
@@ -6,6 +9,7 @@
 #include <netinet/in.h>
 
 using namespace std;
+
 
     class myClient
     {
@@ -72,12 +76,30 @@ using namespace std;
                 }
 
 
-		const char* Message(const char* message)
+		void PostMessage(const char* message)
 		{
 			if (strlen(message) > 1024)
-				return ""; //throw exception or error
+				cout << "Message is too long\n"; //throw exception or error
 			else
-				return message;
+			{
+				if (send(this->ClientSocket, message, strlen(message), 0) == -1)
+				{
+					cout << "Message failed to send\n";
+				}
+				else
+				{
+					char ResponseMessage[256] = {0};
+                                	if (recv(this->ClientSocket, ResponseMessage, 255, 0) == -1)
+                                	{
+                                         	close(this->ClientSocket);
+                                         	cout << "Message not received";
+                                	}
+                                	else
+                                	{        cout << ResponseMessage << endl;
+                                	}
+
+				}
+			}
 		}
 
 
@@ -111,7 +133,8 @@ using namespace std;
 			}
                 }
 
-		int ConnectWithServer()
+
+		void ConnectWithServer()
 		{	sockaddr_in SvrAddr;
 			SvrAddr.sin_family = AF_INET;
 			SvrAddr.sin_port = htons(this->portNum);
@@ -121,30 +144,76 @@ using namespace std;
 			if (Connection == -1)
 			{
 				close(this->ClientSocket);
-				cout << "Terrible" << endl;
-				return -1; //throw exception or error
+				cout << "Connection with server unsuccessful\n"; //throw exception or error
 			}
 			else
 			{
-				cout << Connection << endl;//use the positive connection value when confirming connection
+				cout <<"Connection Successful" << endl;//use the positive connection value when confirming connection
 								   // in the GUI class
-				return Connection;
 			}
 		}
 
 	};
 
+void MainMenu(myClient& Clt1)
+{
+	int end  = 0;
+	int choice;
+        string author, topic; 
+        string token = "POST_SINGLE";
+        string message;
+	const char* newMessage;
+	char* temp;
 
+	while (end = 0) {
+		cout << "\n==== Main Menu ====\n";
+		cout << "1. Write a single post to the discussion board\n";
+		cout << "2. Write multiple posts to the discussion board\n";
+		cout << "3. See all posts on the board\n";
+		cout << "4. Sort posts (filter posts)\n";
+		cout << "0. Exit\n";
+		cout << "Enter your choice\n";
+		cin.ignore();
+
+		switch(choice) {
+		case 1:
+			cout << "Enter author: ";
+			getline(cin, author);
+			author.erase(remove_if(author.begin(), author.end(), ::isspace), author.end());
+
+			cout << "Enter topic: ";
+			getline(cin, topic);
+			topic.erase(remove_if(topic.begin(), topic.end(), ::isspace), topic.end());
+			std:transform(topic.begin(), topic.end(), ::toupper);
+
+			cout << "Enter post: ";
+			getline(cin, message);
+
+			newMessage = token + "|" + author + "|" + topic + "|" + message;
+			strcpy(temp, newMessage);
+			Clt1.PostMessage(temp);
+			break;
+		case 2:
+			//multiple post logic here
+			break;
+		case 3:
+			//get posts from server and print;
+			break;
+		case 4:
+			//Introduce filter
+			break;
+		case 0:
+			end = 1;
+			break;
+		}
+	}
+}
 
 int main(void)
 {
 	char name[100] = "Betty";
-	myClient Clt1(30000, name, "1234");
-	const char* message = Clt1.Message("I actually love my morning hot chocolate\nIt keeps me warm and starts my day on the right track\n");
-	cout << message << endl;
-
-	int Logins = Clt1.sendLoginsToServer();
-
-
+	myClient Clt1(56000, name, "1234");
+        Clt1.ConnectWithServer();
+	//Clt1.PostMessage("POST_SINGLE|anonymous|Topic1|My name is sunshine but i'm in love with the rain. Ironic right?");
 	return 0;
 }
